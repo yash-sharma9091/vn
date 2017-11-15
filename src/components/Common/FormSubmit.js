8847dbb063e52c0730eb76ac0a11b745446d282b
yash sharma <yash_sharma@seologistics.com>
2017-11-15 09:36:34 +0530

    WIP on pilot form API

---
+++ src/components/Common/FormSubmit.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { Button } from "reactstrap";
+
+// Form submit component
+class FormSubmit extends Component {
+  	// render
+  	render() {
+    	const {invalid, submitting, buttonSaveLoading, buttonSave, className} = this.props;
+    	return (
+          	<Button type="submit" bsStyle="primary" className={className} disabled={invalid || submitting}>
+            	{submitting ?
+              	(buttonSaveLoading ? buttonSaveLoading : 'Submitting...') :
+              	(buttonSave ? buttonSave : 'Submit')}
+          	</Button>
+    	);
+  	}
+}
+export default FormSubmit;
+// prop checks
+FormSubmit.propTypes = {
+  error: PropTypes.string,  // redux-form general `_error` message
+  invalid: PropTypes.bool,  // redux-form invalid prop
+  submitting: PropTypes.bool,   // redux-form invalid submitting
+  buttonSaveLoading: PropTypes.string, // save button loading text, default is "Submitting..."
+  buttonSave: PropTypes.string,    // save button text, default is "Save"
+};
